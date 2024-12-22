0be51957d201bc6ced123e483d1b71649fb5e2f6
Jaroslaw Jakubczyk <jaroslaw.jakubczyk@gmail.com>
2024-12-22 18:05:51 +0100

    fdsf

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig, loadEnv } from 'vite';
 import { resolve } from 'path';
+import fs from 'fs-extra';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
+    
+    // Kopiuj worker do katalogu public podczas budowania
+    if (mode === 'production') {
+        const workerSrc = resolve(__dirname, 'node_modules/pdfjs-dist/build/pdf.worker.min.js');
+        const workerDest = resolve(__dirname, 'public/pdf.worker.min.js');
+        fs.copySync(workerSrc, workerDest);
+    }
     
     return {
         define: {
